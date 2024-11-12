4409952e6b78542082d3a9c68d7d2bef324c4371
nguyentrongmanh <nguyentrongmanh050298@gmail.com>
2024-11-12 21:21:54 +0700

    change layout

--- resources/views/wedding-sections/calendar.blade.php
+++ resources/views/wedding-sections/calendar.blade.php
@@ -20,10 +20,11 @@
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
+                            <td>&nbsp;</td>
                             <td>1</td>
-                            <td>2</td>
                         </tr>
                         <tr>
+                            <td>2</td>
                             <td>3</td>
                             <td>4</td>
                             <td>5</td>
@@ -32,39 +33,40 @@
                             <td>
                                 <div id="today">8</div>
                             </td>
-                            <td>9</td>
                         </tr>
                         <tr>
+                            <td>9</td>
                             <td>10</td>
                             <td>11</td>
                             <td>12</td>
                             <td>13</td>
                             <td>14</td>
                             <td>15</td>
-                            <td>16</td>
                         </tr>
                         <tr>
+                            <td>16</td>
                             <td>17</td>
                             <td>18</td>
                             <td>19</td>
                             <td>20</td>
                             <td>21</td>
                             <td>22</td>
-                            <td>23</td>
                         </tr>
                         <tr>
+                            <td>23</td>
                             <td>24</td>
                             <td>25</td>
                             <td>26</td>
                             <td>27</td>
                             <td>28</td>
                             <td>29</td>
-                            <td>30</td>
                         </tr>
                         <tr>
+                            <td>30</td>
                             <td>31</td>
-                            <td id="emptydays" colspan="6">&nbsp;</td>
+                            <td id="emptydays" colspan="5">&nbsp;</td>
                         </tr>
+                        
                     </table>
                 </div>
             </div>
