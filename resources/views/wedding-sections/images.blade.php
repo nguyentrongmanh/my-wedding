ce6628f0a0984d409fe3d4fbab0f937fb6a0aff0
nguyentrongmanh <nguyentrongmanh050298@gmail.com>
2024-11-14 12:37:17 +0700

    chảng image

--- resources/views/wedding-sections/images.blade.php
+++ resources/views/wedding-sections/images.blade.php
@@ -10,111 +10,111 @@
         <div class="row gx-2" id="photoGalleryContainer">
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tình yêu là điều kiện trong đó hạnh phúc của người khác là điều cần thiết cho chính bạn</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-right"
-                data-index="0"> <img src="{{ asset('images/wedding/gallerries/image1.jpg') }}" />
+                data-index="0"> <img src="{{ asset('images/wedding/gallerries/image1_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image2.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image2.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image2_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image2_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không chỉ là một danh từ - nó là một động từ; nó còn hơn cả một cảm giác - đó là sự quan tâm, chia sẻ, giúp đỡ, hy sinh</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-left"
                 data-index="1"> <img
-                    src="{{ asset('images/wedding/gallerries/image2.jpg') }}" />
+                    src="{{ asset('images/wedding/gallerries/image2_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image3.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image3.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image3_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image3_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Vì vậy, để được ai đó nhìn thấy đầy đủ và được yêu mến dù thế nào đi nữa — đây là một sự dâng hiến của con người có thể là điều kỳ diệu</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-right"
                 data-index="2"> <img
-                    src="{{ asset('images/wedding/gallerries/image3.jpg') }}" />
+                    src="{{ asset('images/wedding/gallerries/image3_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image4.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image4.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image4_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image4_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không làm cho thế giới quay tròn. Tình yêu là những gì làm cho chuyến đi đáng giá</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-left"
                 data-index="3"> <img
-                    src="{{ asset('images/wedding/gallerries/image4.jpg') }}" />
+                    src="{{ asset('images/wedding/gallerries/image4_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image5.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image5.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image5_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image5_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không có rào cản. Nó nhảy rào, nhảy rào, xuyên tường để đến đích với đầy hy vọng</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-right"
                 data-index="4"> <img
-                    src="{{ asset('images/wedding/gallerries/image5.jpg') }}" />
+                    src="{{ asset('images/wedding/gallerries/image5_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image6.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image6.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image6_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image6_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tôi có thể chinh phục thế giới bằng một tay miễn là bạn đang nắm tay kia</h4></div>`
                 });
             </script>
 
             <div class="col-6 gallery-item section-sub-title mb-2 btn-see-more-gallery" data-aos="fade-left"
                 data-index="5"> <img
-                    src="{{ asset('images/wedding/gallerries/image6.jpg') }}" />
+                    src="{{ asset('images/wedding/gallerries/image6_small.png') }}" />
             </div>
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image7.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image7.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image7_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image7_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tình yêu đích thực đứng về phía nhau trong những ngày tốt đẹp và sát cánh hơn trong những ngày tồi tệ</h4></div>`
                 });
             </script>
 
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image8.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image8.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image8_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image8_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Được ai đó yêu sâu sắc sẽ mang lại cho bạn sức mạnh, trong khi yêu ai đó sâu sắc sẽ cho bạn dũng khí</h4></div>`
                 });
             </script>
 
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image9.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image9.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image9_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image9_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Đối với thế giới, bạn có thể là một người, nhưng với một người, bạn là cả thế giới</h4></div>`
                 });
             </script>
 
             {{-- <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Tôi yêu bạn vì tất cả những gì bạn đang có, tất cả những gì bạn đã có, và tất cả những gì bạn chưa hiện hữu</h4></div>`
                 });
             </script>
 
             <script type="text/javascript">
                 photoGalleries.push({
-                    src: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
-                    thumb: "{{ asset('images/wedding/gallerries/image1.jpg') }}",
+                    src: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
+                    thumb: "{{ asset('images/wedding/gallerries/image1_small.png') }}",
                     subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
                 });
             </script> --}}
