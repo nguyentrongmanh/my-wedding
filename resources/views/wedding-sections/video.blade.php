3c6716ac8e699c4f26a533063e4e50916bd7108a
nguyentrongmanh <nguyentrongmanh050298@gmail.com>
2024-11-18 01:16:26 +0700

    update content

--- resources/views/wedding-sections/video.blade.php
+++ resources/views/wedding-sections/video.blade.php
@@ -4,7 +4,7 @@
             Video Cưới
         </h2>
         <div class="ratio ratio-16x9" data-aos="zoom-in">
-            <iframe width="100%" src="https://youtu.be/FXwoLOMUMsQ"
+            <iframe width="100%" src="https://www.youtube.com/embed/FXwoLOMUMsQ"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
